ec1127bdb665faf89c4579b79aee97d03e4a1fc0
ksthilaire1 <ksthilaire1@ithaca.edu>
2023-09-05 14:25:55 -0400

    Changed some things

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, CreateAccountForm
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/artists')
+def artists():
     user = {'username': 'Miguel'}
     posts = [
         {
@@ -28,12 +43,3 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
-
-@app.route('/create_user', methods=['GET','POST'])
-def create_user():
-    form = CreateAccountForm()
-    if form.validate_on_submit():
-        flash("New User {} Created".format(form.username.data))
-        return redirect(url_for('login'))
-    return render_template('create_account.html', title="Create User")
-
