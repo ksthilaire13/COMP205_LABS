fc52353e9a46c2988a565addc54558f09b17285a
ksthilaire1 <ksthilaire1@ithaca.edu>
2023-09-14 14:37:28 -0400

    Finished Lab 3

--- app/routes.py
+++ app/routes.py
@@ -14,13 +14,13 @@ def artists():
     return render_template('artists.html', title='Artists', posts=artists_list)
 
 
-@app.route('/new_artist')
+@app.route('/new_artist', methods=['GET', 'POST'])
 def new_artist():
     form = CreateArtistForm()
     if form.validate_on_submit():
-        flash('New Artist Created: {}'.format(
-            form.artist_name.data))
-        return redirect(url_for('artist_v2.html'), info=form)
+        flash('New Artist Created: {}'.format(form.artist_name.data))
+        artists_list.append({'artist': form.artist_name.data, 'hometown': form.artist_hometown.data, 'description': form.artist_description.data, 'events': ['none']})
+        return render_template('artist_page.html', title='Artist Page', user=form.artist_name.data + ",", posts=artists_list)
     return render_template('new_artist.html', title="New Artist", form=form)
 
 
@@ -33,21 +33,25 @@ def artist_page():
 artists_list = [
     {
         'artist': 'John Brown\'s Body',
+        'hometown': 'Ithaca',
         'description': 'Description wooooo!',
         'events': ['an event', 'another event']
     },
     {
         'artist': 'The Gunpoets',
+        'hometown': 'Ithaca',
         'description': 'Voice as a weapon, words as bullets, spreading the universal message of peace, love, and justice through music. Sure, there\'s a cynical cultural tendency to make certain assumptions when you hear the word \"gun\" associated with rap music, but this seven-member live hip-hop band from Ithaca, NY, runs contrary to that image with their positive message and uplifting performances.',
         'events': ['The Haunt next Friday 9/14', 'The Commons on Thursday 9/6'],
     },
     {
         'artist': 'Donna The Buffalo',
+        'hometown': 'Ithaca',
         'description': 'Description blah blah blah',
         'events': ['event 1', 'event 2']
     },
     {
         'artist': 'The Blind Spots',
+        'hometown': 'Ithaca',
         'description': 'Description blah blah blah pt 2',
         'events': ['event 3', 'event 4']
     }
