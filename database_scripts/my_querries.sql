5a4fc3016e1397e369cf72c61daa316710ec3fd1
ksthilaire1 <ksthilaire1@ithaca.edu>
2023-09-25 13:58:21 -0400

    Finished Lab 4

--- database_scripts/my_querries.sql
+++ database_scripts/my_querries.sql
@@ -8,3 +8,9 @@ select e.event_name, e.event_date, v.venue_address, v.max_capacity
 from events e
 join venues v on e.venue_id = v.venue_id
 order by e.event_name;
+
+-- shows all billy bakes events and dates
+select event_name, event_date, artist_name from events
+join artists a on events.artist_id = a.artist_id
+where artist_name = "Billy Bakes"
+order by artist_name;
