66fa2701812aaed2244f6098c265c2dca3a5924d
ksthilaire13 <123573330+ksthilaire13@users.noreply.github.com>
2023-10-06 17:04:56 -0400

    project is broken.

--- app/models.py
+++ app/models.py
@@ -35,11 +35,13 @@ class Venue(db.Model):
 
 
 class Event(db.Model):
-    artist_id = db.Column(db.Integer, primary_key=True)
-    artist_name = db.Column(db.String(128), index=True, unique=True)
-    hometown = db.Column(db.String(128), index=True, unique=True)
-    genre = db.Column(db.String(128), index=True, unique=True)
-    description = db.Column(db.Integer, index=True, unique=True)
+    event_id = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(128), index=True, unique=True)
+    event_date = db.Column(db.String(128), index=True, unique=True)
+    event_description = db.Column(db.String(128), index=True, unique=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('venue.venue_id'))
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.artist_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
     def __repr__(self):
-        return '<Event: {}>'.format(self.venue_name)
+        return '<Event: {}>'.format(self.event_name)
